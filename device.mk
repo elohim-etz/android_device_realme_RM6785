9fd67a4f97093bad8c88610037019694267a1dda
Lunarixus <Nathan@lunarixus.dev>
2023-10-19 04:56:46 +0530

    RM6785: Remove unwanted GApps Packages
    Signed-off-by: Naveen <imnaveenbisht@gmail.com>

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor:64
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
