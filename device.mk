e0e4a11f8b6d450cc181ff91f59b07a59a17d567
SamarV-121 <samar@samarv121.dev>
2024-06-05 09:14:57 +0530

    RM6785: Switch to AOSP charger
    Change-Id: I4ec8d13b4d391788ba4b2968f44c72d401ebf0eb

--- device.mk
+++ device.mk
@@ -58,10 +58,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_camera_metadata
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images_vendor
-
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
@@ -104,8 +100,9 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.mediatek \
-    android.hardware.health-service.mediatek-recovery
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
