9ca66c490514ce8bcb5682a34ca0fd67a8b9cad8
Naveen <imnaveenbisht@gmail.com>
2025-02-11 21:09:56 +0530

    RM6785: Remove more gapps
    Signed-off-by: Naveen <imnaveenbisht@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -6,25 +6,34 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
           arcore \
+          AdaptiveVPNPrebuilt \
           AmbientSensePrebuilt \
           AppDirectedSMSService \
           ConnMO \
           CalendarGooglePrebuilt \
+          CarrierMetrics \
+          Chrome \
           Drive \
           DCMO \
           DevicePolicyPrebuilt \
           DMService \
+          FM2 \
           GCS \
           GoogleCamera \
           MaestroPrebuilt \
           Maps \
+          MeetPrebuilt \
           MicropaperPrebuilt \
           MyVerizonServices \
           OBDM_Permissions \
           obdm_stub \
+          Papers \
+          Photos \
           PixelWallpapers2021 \
           PixelWallpapers2020 \
+          PixelLiveWallpaperPrebuilt \
           RecorderPrebuilt \
+          SafetyHubPrebuilt \
           SCONE \
           ScribePrebuilt \
           Showcase \
