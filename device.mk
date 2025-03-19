cf50f00baaeb35106f9601f5828e7e2b61cdef8b
Giovanni Ricca <giovanniricca@proton.me>
2025-03-19 07:29:11 +0530

    RM6785: Copy audio policy volumes from AOSP
    > audio_policy_volumes.xml: Contains extra volume streams.
    
    Change-Id: I491d015dd0e64bc778fa3ba0d54b76d4efc876ac
    Signed-off-by: Naveen <imnaveenbisht@gmail.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
