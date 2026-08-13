2b6367a67dacc3f3cf40c14614b31b54704eecea
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-08-13 11:56:07 +0530

    RM6785: Build some OMX libs from source
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -200,6 +200,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor:64
 
+# OMX
+PRODUCT_PACKAGES += \
+    libstagefright_softomx.vendor \
+    libstagefright_softomx_plugin.vendor
+
 # Overlays
 $(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 
